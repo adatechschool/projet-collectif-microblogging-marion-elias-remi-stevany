2ed4068e3cecf1e68684a4abe0e13ea34800e248
StevanyF <stevanym96@gmail.com>
2024-04-12 13:12:05 +0000

    Routes and views for Posts and NewPosts

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\NewPostController;
+use App\Models\Post;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,10 +25,22 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/posts', function () {
+    $posts = Post::all(); // Récupérer tous les posts
+    return view('posts.index', compact('posts') );
+});
+
+// Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+
 Route::middleware('auth')->group(function () {
+
+   
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/posts/create', [NewPostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [NewPostController::class, 'store'])->name('posts.store');
 
 require __DIR__.'/auth.php';
